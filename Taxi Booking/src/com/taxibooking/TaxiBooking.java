149c62e26b69321ea2de041039a7343ca76f5344
DINESH-2611 <159759453+DINESH-2611@users.noreply.github.com>
2024-04-23 15:01:07 +0530

    Delete Taxi Booking directory

--- Taxi Booking/src/com/taxibooking/TaxiBooking.java
+++
@@ -1,9 +0,0 @@
-package com.taxibooking;
-
-import com.taxibooking.booktaxi.BookTaxiView;
-
-public class TaxiBooking {
-    public static void main(String[] args) {
-        new BookTaxiView().init();
-    }
-}
